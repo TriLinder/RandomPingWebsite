8e53ae4d4dab0c383035f2175ab466c52da37319
TriLinder <82846064+TriLinder@users.noreply.github.com>
2024-04-16 15:43:10 +0200

    Show progress while deleting account

--- app/svelte/components/AccountDeletionLink.svelte
+++ app/svelte/components/AccountDeletionLink.svelte
@@ -1,12 +1,34 @@
 <script lang="ts">
     import { deleteAccount } from "../utils";
 
-    import { Button, Modal, ModalBody, ModalFooter } from '@sveltestrap/sveltestrap';
+    import { Button, Modal, ModalBody, ModalFooter, Spinner } from '@sveltestrap/sveltestrap';
 
     let isModalOpen = false;
+    let deleting = false;
+
+    async function deleteButtonClick() {
+        deleting = true;
+
+        try {
+            await deleteAccount();
+        } catch(error) {
+            alert(error);
+            deleting = false;
+            return;
+        }
+    }
 </script>
 
 <style>
+    .modal-body {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        height: 100%;
+        justify-content: center;
+        align-items: center;
+    }
+    
     button {
         border: 0;
         background-color: transparent;
@@ -16,11 +38,18 @@
 
 <Modal bind:isOpen={isModalOpen} header="Account deletion" toggle={function() {isModalOpen = !isModalOpen}}>
     <ModalBody>
-        Are you sure you want to unsubscribe from the notifications and delete all your data?
+        <div class="modal-body">
+            {#if !deleting}
+                Are you sure you want to unsubscribe from the notifications and delete all your data?
+            {:else}
+                Deleting. Please wait. <br>
+                <Spinner type="border" color="primary"/>
+            {/if}
+        </div>
     </ModalBody>
     <ModalFooter>
         <Button color="secondary" on:click={function() {isModalOpen = false}}>Cancel</Button>
-        <Button color="danger" on:click={deleteAccount}>Confirm</Button>
+        <Button color="danger" on:click={deleteButtonClick} bind:disabled={deleting}>Confirm</Button>
     </ModalFooter>
 </Modal>
 
